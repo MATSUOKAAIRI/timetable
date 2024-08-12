ac0ef51bc183cc16febf35b3e098e8bfb2e984b6
MATSUOKAAIRI <c1419948@st.kanazawa-it.ac.jp>
2024-08-12 21:32:06 +0900

    first

---
+++ src/app/page.tsx
@@ -0,0 +1,84 @@
+'use client';
+const TableComponent = () => (
+  <table>
+    <tbody>
+      <tr>
+        <td>Content</td>
+      </tr>
+    </tbody>
+  </table>
+);
+
+export default function Home() {
+  return (
+ <div>
+  <div className="bg_pattern Paper_v2">
+  
+    <h1 className="h1">時間割</h1>
+
+    <table className="design08">
+      <tbody>
+        <tr>
+         <th> </th>
+         <th>月曜日</th>
+         <th>火曜日</th>
+         <th>水曜日</th>
+         <th>木曜日</th>
+         <th>金曜日</th>
+         </tr>
+         </tbody>
+         <tbody>
+        <tr>
+         <td>1</td>
+         <td><Form /></td>
+         <td>F</td>
+         <td>g</td>
+         <td>h</td>
+         <td>q</td>
+        </tr>
+        </tbody>
+        <tbody>
+        <tr>
+         <td>2</td>
+         <td>H</td>
+         <td>I</td>
+         <td>b</td>
+         <td>a</td>
+         <td>q</td>
+        </tr>
+        </tbody>
+        <tbody>
+        <tr>
+         <td>3</td>
+         <td>K</td>
+         <td>L</td>
+         <td>a</td>
+         <td>e</td>
+         <td>q</td>
+        </tr>
+        </tbody>
+        <tbody>
+        <tr>
+          <td>4</td>
+          <td>m</td>
+          <td>n</td>
+          <td>w</td>
+          <td>q</td>
+          <td>z</td>
+        </tr>
+        </tbody>
+        <tbody>
+        <tr>
+            <td>5</td>
+            <td>o</td>
+            <td>p</td>
+            <td>y</td>
+            <td>n</td>
+            <td>o</td>
+        </tr>
+        </tbody>
+    </table>
+   </div> 
+   </div>
+  );
+}
